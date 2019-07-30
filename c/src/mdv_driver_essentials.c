2523cb8d22404671ff2d7378a50ca06525c37400
Tuomas Terho <tuomas@terho.net>
2019-07-30 17:27:52 +0300

    Added driver essentials to the SPI API (#33)
    - Added an API function to initialize the essential driver interface.
    - Modified the SPI API to take the driver essentials in use.
    - Modified the serial port API to take the essential driver interface initialization in use.

---
+++ c/src/mdv_driver_essentials.c
@@ -0,0 +1,78 @@
+/***************************************************************************//**
+**
+**  @file       mdv_driver_essentials.c
+**  @ingroup    madivaru-lib
+**  @brief      Essential driver features.
+**  @copyright  Copyright (c) Tuomas Terho. All rights reserved.
+**
+*******************************************************************************/
+/*
+**  BSD 3-Clause License
+**
+**  COPYRIGHT (c) Tuomas Terho
+**  All rights reserved.
+**
+**  Redistribution and use in source and binary forms, with or without
+**  modification, are permitted provided that the following conditions are met:
+**
+**  * Redistributions of source code must retain the above copyright notice,
+**    this list of conditions and the following disclaimer.
+**
+**  * Redistributions in binary form must reproduce the above copyright notice,
+**    this list of conditions and the following disclaimer in the documentation
+**    and/or other materials provided with the distribution.
+**
+**  * Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+**  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+**  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+**  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+**  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+**  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+**  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+**  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+**  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+**  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+**  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+**  POSSIBILITY OF SUCH DAMAGE.
+**
+*******************************************************************************/
+
+#include "mdv_driver_essentials.h"
+
+/******************************************************************************\
+**
+**  API FUNCTION DEFINITIONS
+**
+\******************************************************************************/
+
+MdvResult_t
+mdv_setup_driver_essentials(
+        MdvDriverEssentials_t *essentials,
+        MdvDriverInstance_t *instance,
+        MdvDriverInterface_Init_t funcInit,
+        MdvDriverInterface_Open_t funcOpen,
+        MdvDriverInterface_Close_t funcClose,
+        MdvDriverInterface_Sleep_t funcSleep,
+        MdvDriverInterface_Wakeup_t funcWakeup
+)
+{
+        if(!essentials||!instance){
+                return MDV_DRIVER_ERROR_INVALID_POINTER;
+        }
+        if(!funcInit||!funcOpen||!funcClose||!funcSleep||!funcWakeup){
+                return MDV_DRIVER_ERROR_INVALID_PARAMETER;
+        }
+        essentials->instance=instance;
+        essentials->funcInit=funcInit;
+        essentials->funcOpen=funcOpen;
+        essentials->funcClose=funcClose;
+        essentials->funcSleep=funcSleep;
+        essentials->funcWakeup=funcWakeup;
+        essentials->initialized=true;
+        return MDV_RESULT_OK;
+}
+
+/* EOF */
--- c/src/mdv_spi.c
+++ c/src/mdv_spi.c
@@ -3,39 +3,39 @@
 **  @file       mdv_spi.c
 **  @ingroup    madivaru-lib
 **  @brief      SPI communication API.
-**  @copyright  Copyright (C) 2012-2018 Tuomas Terho. All rights reserved.
+**  @copyright  Copyright (c) Tuomas Terho. All rights reserved.
 **
 *******************************************************************************/
 /*
 **  BSD 3-Clause License
 **
-**  COPYRIGHT (c) 2012-2018, Tuomas Terho
+**  COPYRIGHT (c) Tuomas Terho
 **  All rights reserved.
 **
 **  Redistribution and use in source and binary forms, with or without
 **  modification, are permitted provided that the following conditions are met:
-**  
-**  * Redistributions of source code must retain the above copyright notice, 
+**
+**  * Redistributions of source code must retain the above copyright notice,
 **    this list of conditions and the following disclaimer.
-**  
+**
 **  * Redistributions in binary form must reproduce the above copyright notice,
 **    this list of conditions and the following disclaimer in the documentation
 **    and/or other materials provided with the distribution.
-**  
+**
 **  * Neither the name of the copyright holder nor the names of its
 **    contributors may be used to endorse or promote products derived from
 **    this software without specific prior written permission.
-**  
+**
 **  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 **  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-**  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-**  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-**  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-**  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-**  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-**  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-**  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-**  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+**  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+**  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+**  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+**  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+**  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+**  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+**  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+**  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 **  POSSIBILITY OF SUCH DAMAGE.
 **
 *******************************************************************************/
@@ -51,9 +51,6 @@
 MdvResult_t
 mdv_spi_setup_driver_interface(
         MdvSpi_t *spi,
-        MdvSpiDriverInterface_Init_t funcInit,
-        MdvSpiDriverInterface_Open_t funcOpen,
-        MdvSpiDriverInterface_Close_t funcClose,
         MdvSpiDriverInterface_SelectSlave_t funcSelectSlave,
         MdvSpiDriverInterface_TransferByte_t funcTransferByte,
         MdvSpiDriverInterface_Transfer_t funcTransfer
@@ -62,16 +59,9 @@ mdv_spi_setup_driver_interface(
         if(!spi){
                 return MDV_SPI_ERROR_INVALID_POINTER;
         }
-        if(!funcInit||!funcOpen||!funcClose){
-                return MDV_SPI_ERROR_INVALID_PARAMETER;
-        }
-        spi->drv.funcInit=funcInit;
-        spi->drv.funcOpen=funcOpen;
-        spi->drv.funcClose=funcClose;
         spi->drv.funcSelectSlave=funcSelectSlave;
         spi->drv.funcTransferByte=funcTransferByte;
         spi->drv.funcTransfer=funcTransfer;
-        spi->drv.init=true;
         return MDV_RESULT_OK;
 }
 
@@ -84,17 +74,20 @@ mdv_spi_open(
         MdvHandle_t *handle
 )
 {
+        MdvResult_t result;
+
         if(!spi||!config||!handle){
                 return MDV_SPI_ERROR_INVALID_POINTER;
         }
-        if(!spi->drv.init){
+        if(!spi->drv.essentials.initialized){
                 return MDV_SPI_ERROR_DRIVER_INITIALIZATION_FAILED;
         }
         if(*handle){
                 return MDV_SPI_ERROR_INVALID_PARAMETER;
         }
         // Initialize the spi.
-        if(!MDV_SUCCESSFUL(spi->drv.funcInit(config))){
+        result=spi->drv.essentials.funcInit(&(spi->drv.essentials.instance));
+        if(!MDV_SUCCESSFUL(result)){
                 return MDV_SPI_ERROR_DRIVER_INITIALIZATION_FAILED;
         }
         // Store the port configuration.
@@ -102,8 +95,15 @@ mdv_spi_open(
         // Set the transfer completion callback and user data.
         spi->cbk=callback;
         spi->ud=userData;
-        // Open the port.
-        spi->drv.funcOpen();
+        // Setup the port hardware and open the port.
+        result=spi->drv.essentials.funcWakeup(&(spi->drv.essentials.instance));
+        if(!MDV_SUCCESSFUL(result)){
+                return MDV_SPI_ERROR_DRIVER_INITIALIZATION_FAILED;
+        }
+        result=spi->drv.essentials.funcOpen(&(spi->drv.essentials.instance));
+        if(!MDV_SUCCESSFUL(result)){
+                return MDV_SPI_ERROR_DRIVER_INITIALIZATION_FAILED;
+        }
         // Set the handle to point to the spi.
         *handle=(MdvHandle_t)spi;
         return MDV_RESULT_OK;
@@ -115,7 +115,7 @@ mdv_spi_close(
 )
 {
         MdvSpi_t *spi;
-        
+
         if(!handle){
                 return MDV_SPI_ERROR_INVALID_POINTER;
         }
@@ -125,7 +125,8 @@ mdv_spi_close(
         // Get an access to the driver.
         spi=(MdvSpi_t*)*handle;
         // Close the port.
-        spi->drv.funcClose();
+        spi->drv.essentials.funcClose(&(spi->drv.essentials.instance));
+        spi->drv.essentials.funcSleep(&(spi->drv.essentials.instance));
         // Set the handle to null.
         *handle=(MdvHandle_t)0;
         return MDV_RESULT_OK;
@@ -138,19 +139,24 @@ mdv_spi_select_slave(
 )
 {
         MdvSpi_t *spi;
-        
+        MdvResult_t result;
+
         if(!handle){
                 return MDV_SPI_ERROR_INVALID_PARAMETER;
         }
         // Get an access to the driver.
         spi=(MdvSpi_t*)handle;
-        // Check the port configuration. The operation is not allowed for 
+        // Check the port configuration. The operation is not allowed for
         // a port configured as a slave.
         if(spi->cfg.operatingMode==MDV_SPI_OPERATING_MODE_SLAVE){
                 return MDV_SPI_ERROR_INVALID_OPERATION;
         }
         // Select the slave.
-        if(!MDV_SUCCESSFUL(spi->drv.funcSelectSlave(slaveAddress))){
+        result=spi->drv.funcSelectSlave(
+                &(spi->drv.essentials.instance),
+                slaveAddress
+        );
+        if(!MDV_SUCCESSFUL(result)){
                 return MDV_SPI_ERROR_INVALID_PARAMETER;
         }
         return MDV_RESULT_OK;
@@ -168,7 +174,7 @@ mdv_spi_transfer(
         MdvSpi_t *spi;
         uint8_t tmp=0;
         MdvResult_t result;
-        
+
         if(!dout||!din){
                 return MDV_SPI_ERROR_INVALID_POINTER;
         }
@@ -180,8 +186,14 @@ mdv_spi_transfer(
         // Check if the driver is capable of to transfer all data at once.
         if(spi->drv.funcTransfer){
                 // Transfer data.
-                result=spi->drv.funcTransfer(dout,outsz,din,insz);
-                // If the driver doesn't support inequal buffer sizes, it 
+                result=spi->drv.funcTransfer(
+                        &(spi->drv.essentials.instance),
+                        dout,
+                        outsz,
+                        din,
+                        insz
+                );
+                // If the driver doesn't support inequal buffer sizes, it
                 // returns this error code.
                 if(result==MDV_SPI_ERROR_INVALID_BUFFER_SIZE){
                         return result;
@@ -191,15 +203,20 @@ mdv_spi_transfer(
                         return MDV_SPI_ERROR_TRANSMISSION_FAILED;
                 }
                 return MDV_RESULT_OK;
-        }        
-        // Both Transfer and TransferByte implementations are missing. This is 
+        }
+        // Both Transfer and TransferByte implementations are missing. This is
         // an initialization error.
         if(!spi->drv.funcTransferByte){
                 return MDV_SPI_ERROR_DRIVER_INITIALIZATION_FAILED;
         }
         // Transfer all bi-directional data.
         while(outsz&&insz){
-                if(!MDV_SUCCESSFUL(spi->drv.funcTransferByte(*dout,din))){
+                result=spi->drv.funcTransferByte(
+                        &(spi->drv.essentials.instance),
+                        *dout,
+                        din
+                );
+                if(!MDV_SUCCESSFUL(result)){
                         return MDV_SPI_ERROR_TRANSMISSION_FAILED;
                 }
                 dout++;
@@ -209,7 +226,12 @@ mdv_spi_transfer(
         }
         // Transfer remaining output data (if any).
         while(outsz){
-                if(!MDV_SUCCESSFUL(spi->drv.funcTransferByte(*dout,&tmp))){
+                result=spi->drv.funcTransferByte(
+                        &(spi->drv.essentials.instance),
+                        *dout,
+                        &tmp
+                );
+                if(!MDV_SUCCESSFUL(result)){
                         return MDV_SPI_ERROR_TRANSMISSION_FAILED;
                 }
                 dout++;
@@ -217,7 +239,12 @@ mdv_spi_transfer(
         }
         // Transfer remaining input data (if any).
         while(insz){
-                if(!MDV_SUCCESSFUL(spi->drv.funcTransferByte(tmp,din))){
+                result=spi->drv.funcTransferByte(
+                        &(spi->drv.essentials.instance),
+                        tmp,
+                        din
+                );
+                if(!MDV_SUCCESSFUL(result)){
                         return MDV_SPI_ERROR_TRANSMISSION_FAILED;
                 }
                 din++;
